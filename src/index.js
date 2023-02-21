f6857893252b59c78b33a141fc9ae9c44c51751f
Darcy Christ <darcy@aporia.info>
2023-02-22 10:39:40 +1100

    adjust zoom for Incense Box

--- src/index.js
+++ src/index.js
@@ -67,7 +67,7 @@ $(window).on('scroll', function() {
     var scroll = $(window).scrollTop();
     $(".scrolling-zoom img").css({
         //transform: 'translate3d(-50%, -'+(scroll/100)+'%, 0) scale('+(100 + scroll/5)/100+')',
-        transform: 'translate3d(0, 0, 0) scale('+(100 + scroll/5)/100+')',
+        transform: 'translate3d(0, 0, 0) scale('+(100 + scroll/1)/100+')',
         //Blur suggestion from @janwagner: https://codepen.io/janwagner/ in comments
         //"-webkit-filter": "blur(" + (scroll/200) + "px)",
         //filter: "blur(" + (scroll/200) + "px)"
